ae4e132a06da42f25c0ff7d9e5c82ec9785595c4
dudamollica <dudarenno@yahoo.com.br>
2023-01-28 15:25:02 -0300

    Feat:Add e Deletar produtos e categorias

--- src/controller/categoriesController.js
+++ src/controller/categoriesController.js
@@ -1,13 +1,47 @@
 import db from "../config/database.js";
 
-export async function getCategories(req, res){
-  const findCategories = await db.collection("categories").find({}).toArray()
-  const allCategories = findCategories.map((c)=>c.name)
-  res.send(allCategories.sort())
+export async function getCategories(req, res) {
+  const findCategories = await db.collection("categories").find({}).toArray();
+  const allCategories = findCategories.map((c) => c.name);
+  res.send(allCategories.sort());
 }
 
-export async function showCategoryPage(req, res){
-  const category = req.params.id
-  const categoryProducts = await db.collection("products").find({category:category}).toArray()
-  res.send(categoryProducts)
+export async function showCategoryPage(req, res) {
+  const category = req.params.id;
+  const categoryProducts = await db
+    .collection("products")
+    .find({ category: category })
+    .toArray();
+  res.send(categoryProducts);
+}
+
+export async function addCategory(req, res) {
+  const { name } = req.body;
+
+  try {
+    const duplicateCategory = await db
+      .collection("categories")
+      .findOne({ name });
+    if (duplicateCategory) return res.status(409).send("Categoria já existe!");
+
+    await db.collection("categories").insertOne({ name });
+    const findCategories = await db.collection("categories").find({}).toArray();
+    const allCategories = findCategories.map((c) => c.name);
+    res.send(allCategories.sort());
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function editCategory(req, res) {}
+
+export async function deleteCategory(req, res) {
+  const { name } = req.params;
+
+  try {
+    await db.collection("categories").deleteOne({ name });
+    res.status(202)
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 }
--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { getCategories, showCategoryPage } from "../controller/categoriesController.js";
+import {
+  getCategories,
+  showCategoryPage,
+  addCategory,
+  editCategory,
+  deleteCategory,
+} from "../controller/categoriesController.js";
 
 const categoriesRouter = express.Router();
 
 categoriesRouter.get("/categories", getCategories);
 categoriesRouter.get("/category/:id?", showCategoryPage);
+categoriesRouter.post("/admin-category", addCategory);
+categoriesRouter.delete("/admin-category/:name", deleteCategory);
+categoriesRouter.put("/admin-category", editCategory);
 
 export default categoriesRouter;
